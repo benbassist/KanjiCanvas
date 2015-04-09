72c2b8da082235233ea70543c45102e48c8711ff
Carlo Julian <carlo.julian@gmail.com>
2015-04-08 23:43:53 -0400

    adding some regexs to parse path data

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 var jsdom = require("jsdom");
 var fs = require("fs");
 var jquery = fs.readFileSync("./jquery-1.11.2.min.js", "utf-8");
+var RXPathCommand = /([MmCcLlSsZzHhVvQqTtAa])[\s\d.,-]+/g;
+var RXCommandCoord = /-?\d+(\.\d+)?/;
+var RXCommandCoordPair = /-?\d+(\.\d+)?(\s*,?)-?\d+(\.\d+)?/;
 
 jsdom.env({
     //url: "http://news.ycombinator.com/",
@@ -10,7 +13,15 @@ jsdom.env({
     done: function (errors, window) {
         var $ = window.$;
         $("path").each(function () {
-            console.log($(this).attr('d'));
+            //console.log($(this).attr('d'));
+            //var result = $(this).attr('d').match(RXPathCommand);
+            //console.log(result);
+
+            result=null;
+            while(result = RXPathCommand.exec($(this).attr('d'))) {
+                console.log(result[0]);
+                console.log('type: ' + result[1]);
+            }
         });
     }
 });

38d3756705f18f08f1283c96dd76aca3618e3e5d
Carlo Julian <carlo.julian@gmail.com>
2015-04-10 00:12:43 -0400

    adding kanji

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 // Print all of the news items on Hacker News
-var jsdom = require("jsdom");
-var fs = require("fs");
-var jquery = fs.readFileSync("./jquery-1.11.2.min.js", "utf-8");
-var RXPathCommand = /([MmCcLlSsZzHhVvQqTtAa])[\s\d.,-]+/g;
-var RXCommandCoord = /-?\d+(\.\d+)?/;
-var RXCommandCoordPair = /-?\d+(\.\d+)?(\s*,?)-?\d+(\.\d+)?/;
+var jsdom = require("jsdom"),
+    fs = require("fs"),
+    jquery = fs.readFileSync("./jquery-1.11.2.min.js", "utf-8"),
+    RXPathCommand = /([MmCcLlSsZzHhVvQqTtAa])[\s\d.,-]+/g,
+    RXCommandCoord = /-?\d+(\.\d+)?/,
+    RXCommandCoordPair = /-?\d+(\.\d+)?(\s*,?)-?\d+(\.\d+)?/;
 
 jsdom.env({
-    //url: "http://news.ycombinator.com/",
     file: './kanjivg/0f9a8.svg',
     src: [jquery],
-    done: function (errors, window) {
-        var $ = window.$;
-        $("path").each(function () {
-            //console.log($(this).attr('d'));
-            //var result = $(this).attr('d').match(RXPathCommand);
-            //console.log(result);
-
-            result=null;
-            while(result = RXPathCommand.exec($(this).attr('d'))) {
-                console.log(result[0]);
-                console.log('type: ' + result[1]);
-            }
-        });
-    }
+    done: parseKanji
 });
+
+// TODO:
+// - parse start & end point of each path
+// - calculate angle of stroke
+// - calculate area of stroke bounding box
+// - calculate stroke origin?
+// - calculate bounding box origin?
+function parseKanji(errors, window) {
+    var $ = window.$;
+    $("path").each(function () {
+        //console.log($(this).attr('d'));
+        //var result = $(this).attr('d').match(RXPathCommand);
+        //console.log(result);
+
+        var result = null;
+        while(result = RXPathCommand.exec($(this).attr('d'))) {
+            console.log(result[0]);
+            console.log('type: ' + result[1]);
+        }
+    });
+}

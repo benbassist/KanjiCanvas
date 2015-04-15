7c0a2f13454b00554cdec6f6fa178a006acc5dde
Carlo Julian <carlo.julian@gmail.com>
2015-04-14 21:11:14 -0400

    breaking down each path into coords

--- index.js
+++ index.js
@@ -39,13 +39,37 @@ function parseKanjiChar(svg) {
 // parses stroke data from kanjivg SVG file
 function getStrokeData(svg) {
     var RXPathCommand = /([MmCcLlSsZzHhVvQqTtAa])[\s\d.,-]+/g,
-        RXCommandCoord = /-?\d+(\.\d+)?/,
+        RXCommandCoord = /-?\d+(\.\d+)?/g,
         RXCommandCoordPair = /-?\d+(\.\d+)?(\s*,?)-?\d+(\.\d+)?/;
-    var RX = /<path.*\sd="(.*)".*\/>/g;
-    var result = '';
-    var strokes;
-    while (strokes = RX.exec(svg)) {
-        result += strokes[1] + '\n';
+    var RX = /<path.*\sd="(.*)".*\/>/g,
+        result = '',
+        path = '',
+        command = '',
+        coord = '';
+
+    // in each SVG find all paths
+    while (path = RX.exec(svg)) {
+        //result += path[1] + '\n';
+
+        // in each path find all commands
+        while (command = RXPathCommand.exec(path[1])) {
+            //result += command[0] + '\n';
+
+
+            // in each command find all coords
+            var type = command[1];
+            while (coord = RXCommandCoord.exec(command[0])) {
+                result += coord[0] + '\n';
+            }
+
+            result += '\n';
+            result += '\n';
+        }
+
+        result += '\n';
+        result += '\n';
+        result += '\n';
+        result += '\n';
     }
     return result;
 }

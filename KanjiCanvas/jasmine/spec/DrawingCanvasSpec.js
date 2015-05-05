838009ef798f25d6b66313ead74278dc2b0eeb74
cjulian <carlo.julian@gmail.com>
2015-05-04 20:51:50 -0400

    - renaming CanvasDraw to DrawingCanvas - adding DrawingCanvas spec and fixture

---
+++ KanjiCanvas/jasmine/spec/DrawingCanvasSpec.js
@@ -0,0 +1,60 @@
+(function(){
+    'use strict';
+    describe('Drawing Canvas', function() {
+        var can;
+        beforeEach(function() {
+            loadFixtures('DrawingCanvasFixture.html');
+            can = new DrawingCanvas(document.querySelector('.canvas'));
+        });
+        it('should store a reference to its canvas element', function(){
+            expect(can.c).toExist();
+        });
+        it('should store a reference to its canvas context', function(){
+            expect(can.ctx).toExist();
+        });
+        it('should allow you to register a drawStart callback', function(){
+            var cb = function() { return 'foo' };
+            can.onDrawStart(cb);
+            expect(can.drawStartCallback).toEqual(cb);
+        });
+        it('should allow you to register a draw callback', function(){
+            var cb = function() { return 'foo' };
+            can.onDraw(cb);
+            expect(can.drawCallback).toEqual(cb);
+        });
+        it('should allow you to register a drawEnd callback', function(){
+            var cb = function() { return 'foo' };
+            can.onDrawEnd(cb);
+            expect(can.drawEndCallback).toEqual(cb);
+        });
+        it('should call the drawStartCallback, passing the mousedown coordinates', function(done){
+           var xy,
+               cb = function (x, y) {
+                   xy = {'x': x, 'y': y};
+               };
+            can.onDrawStart(cb);
+            can.drawStart({offsetX:10, offsetY:22, target: { getBoundingClientRect: function(){}}});
+            expect(xy).toEqual({'x': 10, 'y': 22});
+            done();
+        });
+        it('should call the drawCallback, passing the mousemove coordinates', function(done){
+            var xy,
+                cb = function (x, y) {
+                    xy = {'x': x, 'y': y};
+                };
+            can.onDraw(cb);
+            can.draw({offsetX:11, offsetY:23, target: { getBoundingClientRect: function(){}}});
+            expect(xy).toEqual({'x': 11, 'y': 23});
+            done();
+        });
+        it('should call the drawEndCallback', function(){
+            var cb = {
+                foo: function () { return 0; }
+            };
+            spyOn(cb, 'foo');
+            can.onDrawEnd(cb.foo);
+            can.drawEnd({offsetX:11, offsetY:23, target: { getBoundingClientRect: function(){}}});
+            expect(cb.foo).toHaveBeenCalled();
+        });
+    });
+})();
--- KanjiCanvas/jasmine/src/DrawingCanvas.js
+++ KanjiCanvas/jasmine/src/DrawingCanvas.js
@@ -1,23 +1,26 @@
-var CanvasDraw = (function(){
+var DrawingCanvas = (function(){
     'use strict';
-    function CanvasDraw(el) {
+    function DrawingCanvas(el) {
         this.c = el;
         this.ctx = this.c.getContext('2d');
         this.moveHandler = null;
         this.upHandler = null;
         this.outHandler = null;
+        this.drawStartCallback = null;
+        this.drawCallback = null;
+        this.drawEndCallback = null;
         this.c.addEventListener("mousedown", this.drawStart.bind(this), false);
 
         // canvas and line style
         this.c.height = this.c.clientHeight;
-        this.c.width = this.c.clientHeight;
+        this.c.width = this.c.clientWidth;
         this.ctx.lineCap = 'round';
         this.ctx.lineJoin = 'round';
         this.ctx.lineWidth = 3;
     }
 
     // given a mouse event returns mouse coordinates relative to the canvas
-    CanvasDraw.prototype.getOffset = function(e) {
+    DrawingCanvas.prototype.getOffset = function(e) {
         var rect = e.target.getBoundingClientRect();
         return {
             x: e.offsetX || e.pageX - rect.left - window.scrollX,
@@ -26,24 +29,30 @@ var CanvasDraw = (function(){
     };
 
     // initializes canvas path, registers handlers for other mouse events
-    CanvasDraw.prototype.drawStart = function(e) {
+    DrawingCanvas.prototype.drawStart = function(e) {
         var p = this.getOffset(e);
         this.c.addEventListener("mousemove", this.moveHandler = this.draw.bind(this), false);
         this.c.addEventListener("mouseup", this.upHandler = this.drawEnd.bind(this), false);
         this.c.addEventListener("mouseout", this.outHandler = this.drawEnd.bind(this), false);
         this.ctx.beginPath();
         this.ctx.moveTo(p.x, p.y);
+        if(typeof this.drawStartCallback === 'function') {
+            this.drawStartCallback(p.x, p.y);
+        }
     };
 
     // continues the path to the current mouse coordinates and draws it
-    CanvasDraw.prototype.draw = function(e) {
+    DrawingCanvas.prototype.draw = function(e) {
         var p = this.getOffset(e);
         this.ctx.lineTo(p.x, p.y);
         this.ctx.stroke();
+        if(typeof this.drawCallback === 'function') {
+            this.drawCallback(p.x, p.y);
+        }
     };
 
     // closes the path and removes the mouse event handlers
-    CanvasDraw.prototype.drawEnd = function (e) {
+    DrawingCanvas.prototype.drawEnd = function (e) {
         this.draw(e);
         this.ctx.closePath();
         this.c.removeEventListener('mousemove', this.moveHandler);
@@ -51,7 +60,26 @@ var CanvasDraw = (function(){
         this.c.removeEventListener('mouseout', this.outHandler);
         this.upHandler = null;
         this.moveHandler = null;
-    }
+        if(typeof this.drawEndCallback === 'function') {
+            this.drawEndCallback();
+        }
+    };
 
-    return CanvasDraw;
+    DrawingCanvas.prototype.clear = function () {
+        this.ctx.clearRect(0,0,this.c.width, this.c.height);
+    };
+
+    DrawingCanvas.prototype.onDrawStart = function(callback) {
+        this.drawStartCallback = callback;
+    };
+
+    DrawingCanvas.prototype.onDraw = function(callback) {
+        this.drawCallback = callback;
+    };
+
+    DrawingCanvas.prototype.onDrawEnd = function(callback) {
+        this.drawEndCallback = callback;
+    };
+
+    return DrawingCanvas;
 })();

6d6045158fb20e1a3c8967180d1933ab60dbc4cd
Carlo Julian <carlo.julian@gmail.com>
2015-05-06 14:07:03 -0400

    folder rename, cleanup, adding dependencies

--- KanjiCanvas/jasmine/src/KanjiCanvas.js
+++ KanjiCanvas/jasmine/src/KanjiCanvas.js
@@ -1,52 +1,52 @@
-var KanjiCanvas = (function(){
-    'use strict';
+var KanjiCanvas = (function (DrawingCanvas, StrokeList, Stroke, p) {
+	'use strict';
 
-    function KanjiCanvas(el) {
-        this.kc = el;
-        this.canvas = new DrawingCanvas(el.querySelector('canvas'));
-        this.clearBtn = this.kc.querySelector('.clear');
-        this.submitBtn = this.kc.querySelector('.submit');
-        this.strokeList = new StrokeList();
-        this.currStroke = null;
-        this.canvas.onDrawStart(this.drawStartHandler.bind(this));
-        this.canvas.onDraw(this.drawHandler.bind(this));
-        this.canvas.onDrawEnd(this.drawEndHandler.bind(this));
-        this.submitCallback = null;
-        this.clearBtn.addEventListener('click', this.clear.bind(this));
-        this.submitBtn.addEventListener('click', this.submit.bind(this));
-    }
+	function KanjiCanvas(el) {
+		this.kc = el;
+		this.canvas = new DrawingCanvas(el.querySelector('canvas'));
+		this.clearBtn = this.kc.querySelector('.clear');
+		this.submitBtn = this.kc.querySelector('.submit');
+		this.strokeList = new StrokeList();
+		this.currStroke = null;
+		this.canvas.onDrawStart(this.drawStartHandler.bind(this));
+		this.canvas.onDraw(this.drawHandler.bind(this));
+		this.canvas.onDrawEnd(this.drawEndHandler.bind(this));
+		this.submitCallback = null;
+		this.clearBtn.addEventListener('click', this.clear.bind(this));
+		this.submitBtn.addEventListener('click', this.submit.bind(this));
+	}
 
-    KanjiCanvas.prototype.clear = function() {
-        this.canvas.clear();
-        this.strokeList.clear();
-    };
+	KanjiCanvas.prototype.clear = function () {
+		this.canvas.clear();
+		this.strokeList.clear();
+	};
 
-    KanjiCanvas.prototype.submit = function() {
-        if(typeof this.submitCallback === 'function') {
-            this.submitCallback(this.strokeList);
-        }
-    }
+	KanjiCanvas.prototype.submit = function () {
+		if (typeof this.submitCallback === 'function') {
+			this.submitCallback(this.strokeList);
+		}
+	}
 
-    KanjiCanvas.prototype.drawStartHandler = function(x,y){
-        this.currStroke = new Stroke(p(x,y));
-    };
+	KanjiCanvas.prototype.drawStartHandler = function (x, y) {
+		this.currStroke = new Stroke(p(x, y));
+	};
 
-    KanjiCanvas.prototype.drawHandler = function(x, y) {
-        if (this.currStroke) {
-            this.currStroke.add(p(x,y));
-        }
-    };
+	KanjiCanvas.prototype.drawHandler = function (x, y) {
+		if (this.currStroke) {
+			this.currStroke.add(p(x, y));
+		}
+	};
 
-    KanjiCanvas.prototype.drawEndHandler = function(){
-        if (this.currStroke) {
-            this.strokeList.add(this.currStroke);
-        }
-    };
+	KanjiCanvas.prototype.drawEndHandler = function () {
+		if (this.currStroke) {
+			this.strokeList.add(this.currStroke);
+		}
+	};
 
-    KanjiCanvas.prototype.onSubmit = function(callback) {
-        this.submitCallback = callback;
-    };
+	KanjiCanvas.prototype.onSubmit = function (callback) {
+		this.submitCallback = callback;
+	};
 
+	return KanjiCanvas;
 
-    return KanjiCanvas;
-})();
+})(DrawingCanvas, StrokeList, Stroke, p);
--- KanjiCanvas/jasmine/src/Stroke.js
+++ KanjiCanvas/jasmine/src/Stroke.js
@@ -1,66 +1,68 @@
-var Stroke = (function(){
+var Stroke = (function () {
+	'use strict';
+
 	function Stroke() {
 		this.points = arguments.length === 0 ? [] :
-            Array.prototype.slice.call(arguments);
-        this.angle = NaN;
+			Array.prototype.slice.call(arguments);
+		this.angle = NaN;
 	}
 
-	Stroke.prototype.add = function(point) {
+	Stroke.prototype.add = function (point) {
 		this.points.push(point);
-        if(this.points.length > 1) {
-            this.angle = this.getAngle();
-        }
+		if (this.points.length > 1) {
+			this.angle = this.getAngle();
+		}
 	};
 
-    Stroke.prototype.maxX = function() {
-        var i, x = null;
-        for(i = 0; i < this.points.length; i++) {
-            if(x === null || this.points[i].x > x) {
-                x = this.points[i].x;
-            }
-        }
-        return x;
-    };
+	Stroke.prototype.maxX = function () {
+		var i, x = null;
+		for (i = 0; i < this.points.length; i++) {
+			if (x === null || this.points[i].x > x) {
+				x = this.points[i].x;
+			}
+		}
+		return x;
+	};
 
-    Stroke.prototype.minX = function() {
-        var i, x = null;
-        for(i = 0; i < this.points.length; i++) {
-            if(x === null || this.points[i].x < x) {
-                x = this.points[i].x;
-            }
-        }
-        return x;
-    };
+	Stroke.prototype.minX = function () {
+		var i, x = null;
+		for (i = 0; i < this.points.length; i++) {
+			if (x === null || this.points[i].x < x) {
+				x = this.points[i].x;
+			}
+		}
+		return x;
+	};
 
-    Stroke.prototype.maxY = function() {
-        var i, y = null;
-        for(i = 0; i < this.points.length; i++) {
-            if(y === null || this.points[i].y > y) {
-                y = this.points[i].y;
-            }
-        }
-        return y;
-    };
+	Stroke.prototype.maxY = function () {
+		var i, y = null;
+		for (i = 0; i < this.points.length; i++) {
+			if (y === null || this.points[i].y > y) {
+				y = this.points[i].y;
+			}
+		}
+		return y;
+	};
 
-    Stroke.prototype.minY = function() {
-        var i, y = null;
-        for(i = 0; i < this.points.length; i++) {
-            if(y === null || this.points[i].y < y) {
-                y = this.points[i].y;
-            }
-        }
-        return y;
-    };
+	Stroke.prototype.minY = function () {
+		var i, y = null;
+		for (i = 0; i < this.points.length; i++) {
+			if (y === null || this.points[i].y < y) {
+				y = this.points[i].y;
+			}
+		}
+		return y;
+	};
 
-    Stroke.prototype.area = function() {
-        return (this.maxX() - this.minX()) * (this.maxY() - this.minY());
-    }
+	Stroke.prototype.area = function () {
+		return (this.maxX() - this.minX()) * (this.maxY() - this.minY());
+	}
 
-    Stroke.prototype.getAngle = function() {
-        var p1 = this.points[0],
-            p2 = this.points[this.points.length - 1];
-        return Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180 / Math.PI;
-    }
+	Stroke.prototype.getAngle = function () {
+		var p1 = this.points[0],
+			p2 = this.points[this.points.length - 1];
+		return Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180 / Math.PI;
+	}
 
 	return Stroke;
 })();
